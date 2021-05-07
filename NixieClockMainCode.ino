ae99124b74f449fea330ccb38e2b36b0d485924c
devKarthikRaj <rocketraj2000@gmail.com>
2021-05-07 17:30:54 +0800

    Split code into 2 cores, added BCD conversion to write RTC function

--- NixieClockMainCode.ino
+++ NixieClockMainCode.ino
@@ -1,12 +1,17 @@
 //Use NodeMCU-32S for Nixie Hardware
 //Use ESP32 DEV MODULE for esp32 dev board 
 
+//Taskhandlers to handle the tasks for each core
+TaskHandle_t Task0; //Runs on core 0
+TaskHandle_t Task1; //Runs on core 1
+
 //Headers
 //#include <Wire.h> //I2C lib
 #include <pcf2129rtc.h> //RTC lib
 #include "NTC_PCA9698.h" //Port Expander lib
 #include <getNixieExpanderPin.h> //Port Expander to Nixie Digit mapping lib
 #include "BluetoothSerial.h" //Bluetooth lib
+#include <WS2812FX.h> //RGB LED lib
 
 //Pin Definitions 
 #define bootControl     0 //ref to ds - not req atm                                 (NOT USED IN THIS CODE)
@@ -21,10 +26,10 @@
 #define twimIntSDA     19 //I2C SDA
 #define ledBus         21 //WS2812 RGBLED Control Bus
 #define sysLed         22 //System LED - UNUSED FOR NOW 
-#define opsLed         23 //Operation LED for RTC status
+#define opsLed         23 //Operation LED - Blinking if RTC interrupt is triggered and Nixie Tubes are updated
 #define devLed         25 //Dev LED - UNUSED FOR NOW
 #define pwrLed         26 //Power LED for power system (5V & 170V) status
-#define comLed         27 //Comms LED for WiFi connection
+#define comLed         27 //Fast Blinking if not connected to app... Continuous if connected to app
 
 //Defining cross-function vars
 int rtcHour;
@@ -51,29 +56,55 @@ int currentTube6pin = 2000;
 //Creating an instance of the PCF2129 RTC Lib
 pcf2129rtc pcf2129rtcInstance(twimIntSDA, twimIntSCL); //(SDA,SCL)
 
-BluetoothSerial espBt; //Declare BT object
-
-//Unique code that'll be known only to user or will be labelled on the hardware itself
-String uniqueCode = "UNIQUE_CODE";
-
-bool secIntFlag = false; //Seconds Interrupt Flag
+//Seconds Interrupt Flag
+bool secIntFlag = false; 
 //RTC Seconds Interrupt ISR
 void IRAM_ATTR rtcIntISR() {
   secIntFlag = true;
 }
 
-//Create an instance of the port expander libraries to control the port expanders via i2c
+//Declare BT object
+BluetoothSerial espBt;
+
+//Unique code that'll be known only to user or will be labelled on the hardware itself (security)
+String uniqueCode = "UNIQUE_CODE";
+
+//Create 2 instances of the port expander lib to control 2 port expanders via I2C
 PCA9698 expanderChip0(0x20,twimIntSDA,twimIntSCL,1000000); //(I2C_ADDR,SDA,SCL,SPEED)
 PCA9698 expanderChip1(0x21,twimIntSDA,twimIntSCL,1000000); 
 
-//Create an instance of the port expander to nixie tube digit pin mapping lib
+//Create an instance of the port expander to nixie tube digit pin mapping lib 
+//***This lib is specific to the Nixie hardware used in this project***
 getNixieExpanderPin getNixieExpanderPinInstance; 
+
+//Create an instance of the RGB LED lib
+WS2812FX ws2812fx = WS2812FX(6, ledBus, NEO_GRB + NEO_KHZ800); //(LED_COUNT,LED_PIN,NEO_GRB + NEO_KHZ800)
 
 void setup() {
   Serial.begin(115200);
 
+  //Core 0 Config
+  xTaskCreatePinnedToCore(
+    codeForTask0, //Task Function
+    "Task 0",     //Name of Task
+    1000,         //Stack size of task
+    NULL,         //Parameter of the task
+    1,            //Priority of the task
+    &Task0,       //Task handle to keep track of the created task
+    0);           //Target Core
+
+  //Core 1 Config
+  xTaskCreatePinnedToCore(
+    codeForTask1, //Task Function
+    "Task 1",     //Name of Task
+    1000,         //Stack size of task
+    NULL,         //Parameter of the task
+    1,            //Priority of the task
+    &Task1,       //Task handle to keep track of the created task
+    1);           //Target Core
+
   //IO Definitions
-  pinMode(rtcInt,INPUT_PULLUP);
+  pinMode(rtcInt,INPUT_PULLUP); //Interrupt pin has to be set as input pullup
   pinMode(en170V,OUTPUT);
   pinMode(en5V,OUTPUT);
   pinMode(supervisor5V,INPUT);
@@ -84,7 +115,8 @@ void setup() {
   pinMode(pwrLed,OUTPUT);
   pinMode(comLed,OUTPUT);
 
-  enablePowerSupplies(); //Turn on the 5V and 170V supplies (3.3V is auto turned on cuz ESP32 run off it)
+  //Turn on the 5V and 170V supplies (3.3V is auto turned on cuz ESP32 run off it)
+  enablePowerSupplies(); 
 
   //Name of BT signal
   espBt.begin("ESP32_BT_Control"); //This will be the name shown to other BT devices in the BT network
@@ -93,11 +125,12 @@ void setup() {
   pcf2129rtcInstance.rtcInitialConfig(); 
 
   //Update RTC with current time
-  //Manually write the hour,min,sec (for debugging only)
-  int manuallyWrittenHour = 1;
-  int manuallyWrittenMin = 1;
-  int manuallyWrittenSec = 1;
-  pcf2129rtcInstance.updateCurrentTimeToRTC(manuallyWrittenHour, manuallyWrittenMin, manuallyWrittenSec); //(HR,MIN,SEC)
+  //Manually write the default hour, min and sec to RTC 
+  //This will be the default start up time when the clock is reset
+  int defaultHour = 0;
+  int defaultMin = 0;
+  int defaultSec = 0;
+  pcf2129rtcInstance.updateCurrentTimeToRTC(defaultHour, defaultMin, defaultSec); //(HOUR,MIN,SEC)
 
   //Interrupt Definitions
   //attachInterrupt(digitalPinToInterrupt(PIN_NUM), ISR, mode)
@@ -119,69 +152,17 @@ void setup() {
   expanderChip1.portMode(2,OUTPUT);
   expanderChip1.portMode(3,OUTPUT);
   expanderChip1.portMode(4,OUTPUT);
+
+  //RGB LED Config
+  ws2812fx.init();
+  ws2812fx.setBrightness(50);
+  ws2812fx.setSpeed(5);
+  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  ws2812fx.start();
 }
 
-//Core 1
 void loop() {
-  if(secIntFlag == true) {
-    //Get the pin numbers of the nixie digits to turn on for each tube
-    //getNixieExpanderPinInstance.getPinNumber(TUBE_NUM,BCD_DIGIT)
-    writeTube1pin = getNixieExpanderPinInstance.getPinNumber(1,pcf2129rtcInstance.readRtcHourBCD1());
-    writeTube2pin = getNixieExpanderPinInstance.getPinNumber(2,pcf2129rtcInstance.readRtcHourBCD0());
-    writeTube3pin = getNixieExpanderPinInstance.getPinNumber(3,pcf2129rtcInstance.readRtcMinBCD1());
-    writeTube4pin = getNixieExpanderPinInstance.getPinNumber(4,pcf2129rtcInstance.readRtcMinBCD0());
-    writeTube5pin = getNixieExpanderPinInstance.getPinNumber(5,pcf2129rtcInstance.readRtcSecBCD1());
-    writeTube6pin = getNixieExpanderPinInstance.getPinNumber(6,pcf2129rtcInstance.readRtcSecBCD0());
-
-    //If the tube has to be updated...
-    if(writeTube1pin != currentTube1pin) {
-      offNixieTube1(); //Turn off all digits on that tube
-      expanderChip0.digitalWrite(writeTube1pin,HIGH); //Turn on the updated digit
-
-      //Turning off the tube and then updating the tube is done to prevent double digit display on the tube
-    }
-    if(writeTube2pin != currentTube2pin) {
-      offNixieTube2();
-      expanderChip0.digitalWrite(writeTube2pin,HIGH);
-    }
-    if(writeTube3pin != currentTube3pin) {
-      offNixieTube3();
-      expanderChip0.digitalWrite(writeTube3pin,HIGH); 
-    }
-    if(writeTube4pin != currentTube4pin) {
-      offNixieTube4();
-      expanderChip1.digitalWrite(writeTube4pin,HIGH);  
-    }
-    if(writeTube5pin != currentTube5pin) {
-      offNixieTube5();
-      expanderChip1.digitalWrite(writeTube5pin,HIGH);
-    }
-    if(writeTube6pin != currentTube6pin) {
-      offNixieTube6();
-      expanderChip1.digitalWrite(writeTube6pin,HIGH);
-    }
-
-    //Update the tube state (digit currently displayed in each tube)
-    currentTube1pin = writeTube1pin;
-    currentTube2pin = writeTube2pin;
-    currentTube3pin = writeTube3pin;
-    currentTube4pin = writeTube4pin;
-    currentTube5pin = writeTube5pin;
-    currentTube6pin = writeTube6pin;    
-
-    //Reset interrupt secIntFlag
-    secIntFlag = false;
-    pcf2129rtcInstance.clearMsf();
-    
-    //Flash OpsLed to indicate rtc seconds interrupt successful triggering
-    digitalWrite(opsLed,HIGH);
-    delay(50);
-    digitalWrite(opsLed,LOW);
-  }
-  
-  //Until hardware verification is successful... hang at this line...
-  //while(VerifyBtConnection()!=true){};
-  //Serial.println("Hardware verification successful");
+  //Loop isn't used as all the code has been separated into tasks and run explicitly on core 1 and 2
 }
 
 //Function Definitions
@@ -192,21 +173,32 @@ void enablePowerSupplies() {
 }
 
 //Run this function until it returns true
+//Return true = security verification successful
+//Return false = security verification unsuccessful/timeout
 bool VerifyBtConnection() {
-  //Security Trooper State Machine!!! Dedicating this to STs in the SAF hehe :)
   int state = 0; //Set the starting state of the state machine
   for(int i=0; i<3; i++) {
     switch(state) {
       case 0:
       //Hang here till a remote device connects...
       Serial.println("Waiting for connection from remote device...");
-      while(!espBt.hasClient()){};
+      while(!espBt.hasClient()) {
+        digitalWrite(comLed,HIGH);
+        delay(50);
+        digitalWrite(comLed,LOW);
+        delay(50);
+      }
 
       espBt.print("REQ_CHECK_1");
 
       //Hang here till level 1 verification key is received... (auto sent by hardware)
       Serial.println("Waiting for Level 1 verification key from remote device...");
-      while(!espBt.available()){};
+      while(!espBt.available()) {
+        digitalWrite(comLed,HIGH);
+        delay(50);
+        digitalWrite(comLed,LOW);
+        delay(50);
+      }
       
       if(espBt.readString() == "REQ_CONN") {
         Serial.println("Level 1 Pass");
@@ -222,7 +214,12 @@ bool VerifyBtConnection() {
       case 1:
       //Hang here till Level 2 verification key is received... (keyed in by user)
       Serial.println("Waiting for Level 2 verification key from remote device");
-      while(!espBt.available()){};
+      while(!espBt.available()) {
+        digitalWrite(comLed,HIGH);
+        delay(50);
+        digitalWrite(comLed,LOW);
+        delay(50);
+      }
 
       if(espBt.readString() == uniqueCode) {
         Serial.println("Level 2 Pass");
@@ -244,6 +241,7 @@ bool VerifyBtConnection() {
 }
 
 //THIS FUNCTION IS UNTESTED
+//ISR triggered when there is a blackout on the 5V rail
 IRAM_ATTR void pwrSysFailISR() {
   //Check sub system statuses and set LEDs in void setup
   //Use interrupt to toggle LEDs if subsystems fail!!!
@@ -260,14 +258,16 @@ IRAM_ATTR void pwrSysFailISR() {
 }
 
 //THIS FUNCTION IS INCOMPLETE AND UNTESTED
+//This function is called by the pwrSysFailISR() which is triggered when there is a blackout on the 5V rail
+//This function will switch off the 5V and 170V power supplies and turn off the RGB LEDs... 
+//This puts the hardware into safe mode
 void disableSubsystems() {
-  //This fucntion will disable all subsystems when there's an interrupt indicating a sub system fault
-
   //Disable power sub systems
   digitalWrite(en170V, HIGH);
   digitalWrite(en5V, LOW);
 
   //Disable RGB LEDs
+  ws2812fx.stop();
 }
 
 void offNixieTube1() {
@@ -298,5 +298,94 @@ void offNixieTube5() {
 void offNixieTube6() {
   for(int i=0;i<10;i++) {
     expanderChip1.digitalWrite(getNixieExpanderPinInstance.getPinNumber(6,i),LOW);
+  }
+}
+
+//Main Code for Core 0 and 1 
+
+/* Core 0 
+ *  Comms with Project Nixie android app
+ *  Send data received from the app to core 1 so that the Nixies can be updated
+ */
+void codeForTask0(void * parameter) {
+  while(1) {
+    //Until hardware verification is successful... hang at this line...
+    while(VerifyBtConnection()!=true){};  
+    Serial.println("Hardware verification successful");
+    while(1) {
+      digitalWrite(comLed,HIGH);
+      for(int i=0;i<100000;i++){}
+      delay(5);
+    }
+  }
+}
+
+/* Core 1
+ *  Drive Nixies when seconds interrupt is triggered by RTC
+ *  Drive RGB LEDs
+ *  Nixie Tube Cathode Protection (TBD)
+ */
+void codeForTask1(void * parameter) {
+  while(1) {
+    if(secIntFlag == true) {
+      //Get the pin numbers of the nixie digits to turn on for each tube
+      //getNixieExpanderPinInstance.getPinNumber(TUBE_NUM,BCD_DIGIT)
+      writeTube1pin = getNixieExpanderPinInstance.getPinNumber(1,pcf2129rtcInstance.readRtcHourBCD1());
+      writeTube2pin = getNixieExpanderPinInstance.getPinNumber(2,pcf2129rtcInstance.readRtcHourBCD0());
+      writeTube3pin = getNixieExpanderPinInstance.getPinNumber(3,pcf2129rtcInstance.readRtcMinBCD1());
+      writeTube4pin = getNixieExpanderPinInstance.getPinNumber(4,pcf2129rtcInstance.readRtcMinBCD0());
+      writeTube5pin = getNixieExpanderPinInstance.getPinNumber(5,pcf2129rtcInstance.readRtcSecBCD1());
+      writeTube6pin = getNixieExpanderPinInstance.getPinNumber(6,pcf2129rtcInstance.readRtcSecBCD0());
+  
+      //If the tube has to be updated...
+      if(writeTube1pin != currentTube1pin) {
+        offNixieTube1(); //Turn off all digits on that tube
+        expanderChip0.digitalWrite(writeTube1pin,HIGH); //Turn on the updated digit
+        //Turning off the tube and then updating the tube is done to prevent double digit display on the tube
+      }
+      if(writeTube2pin != currentTube2pin) {
+        offNixieTube2();
+        expanderChip0.digitalWrite(writeTube2pin,HIGH);
+      }
+      if(writeTube3pin != currentTube3pin) {
+        offNixieTube3();
+        expanderChip0.digitalWrite(writeTube3pin,HIGH); 
+      }
+      if(writeTube4pin != currentTube4pin) {
+        offNixieTube4();
+        expanderChip1.digitalWrite(writeTube4pin,HIGH);  
+      }
+      if(writeTube5pin != currentTube5pin) {
+        offNixieTube5();
+        expanderChip1.digitalWrite(writeTube5pin,HIGH);
+      }
+      if(writeTube6pin != currentTube6pin) {
+        offNixieTube6();
+        expanderChip1.digitalWrite(writeTube6pin,HIGH);
+      }
+  
+      //Update the tube state (digit currently displayed in each tube)
+      currentTube1pin = writeTube1pin;
+      currentTube2pin = writeTube2pin;
+      currentTube3pin = writeTube3pin;
+      currentTube4pin = writeTube4pin;
+      currentTube5pin = writeTube5pin;
+      currentTube6pin = writeTube6pin;
+  
+      //Reset interrupt secIntFlag
+      secIntFlag = false;
+      pcf2129rtcInstance.clearMsf();
+      
+      //Flash OpsLed to indicate rtc seconds interrupt successful triggering
+      digitalWrite(opsLed,HIGH);
+      delay(50);
+      digitalWrite(opsLed,LOW);
+    }
+    //Code inside the else loop will execute if the core is not busy driving the nixies
+    //Priority is given to driving the nixies!!!
+    else {
+      //Drive the RGB LEDs 
+      ws2812fx.service();
+    }
   }
 }
